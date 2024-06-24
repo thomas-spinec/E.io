234daba2369c1ca8f13c6aff92e223dad060af50
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2024-06-24 11:36:24 +0200

    add front app

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,9 +1,42 @@
+import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+import {Server} from 'socket.io';
 
 const app = express();
 
-const PORT = 3000;
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true
+    } 
+));
+app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+const EXPRESS = process.env.EXPRESS_PORT
+const SOCKET = Number(process.env.SOCKET_PORT)
+
+app.listen(EXPRESS, () => {
+
+    console.log(`Server is running on port ${EXPRESS}`);
+});
+
+const io = new Server(SOCKET, {
+    cors: {
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  
+    socket.emit("message", "Hello World");
+  
+    socket.on('response', (data) => {
+      console.log(data);
+    });    
 });
