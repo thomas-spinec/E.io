5b41bf53e964cb2f6ad9dca6a3d000c29958b333
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2024-06-25 11:47:18 +0200

    chore: Update server configuration and socket event handling

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,42 +1,46 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import {Server} from 'socket.io';
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import { Server } from "socket.io";
 
 const app = express();
 
 app.use(
-    cors({
-        origin: 'http://localhost:5173',
-        credentials: true
-    } 
-));
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-const EXPRESS = process.env.EXPRESS_PORT
-const SOCKET = Number(process.env.SOCKET_PORT)
+const EXPRESS = process.env.EXPRESS_PORT;
+const SOCKET = Number(process.env.SOCKET_PORT);
 
 app.listen(EXPRESS, () => {
-
-    console.log(`Server is running on port ${EXPRESS}`);
+  console.log(`Server is running on port ${EXPRESS}`);
 });
 
 const io = new Server(SOCKET, {
-    cors: {
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  
-    socket.emit("message", "Hello World");
-  
-    socket.on('response', (data) => {
-      console.log(data);
-    });    
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.emit("message", "Hello World");
+
+  socket.on("response", (data) => {
+    console.log(data);
+  });
+  socket.onAny((event, ...args) => {
+    console.log("EVENT, ARG", event, args);
+
+    console.log("SOCKET.AUTH", socket.handshake.auth);
+  });
 });
