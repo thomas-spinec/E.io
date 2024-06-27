e1e4995b6ad110cad8df5d02237e4926e56de406
thomas-spinec <thomas.spinec@laplateforme.io>
2024-06-27 11:35:33 +0200

    chore: Update security configuration for user registration and login

--- API/src/Controller/RegistrationController.php
+++ API/src/Controller/RegistrationController.php
@@ -42,33 +42,6 @@ class RegistrationController extends AbstractController
         $entityManager->persist($user);
         $entityManager->flush();
 
-
-
         return $this->json($user, 201);
-
-
-        // $form = $this->createForm(RegistrationFormType::class, $user);
-        // $form->handleRequest($request);
-
-        // if ($form->isSubmitted() && $form->isValid()) {
-        //     // encode the plain password
-        //     $user->setPassword(
-        //         $userPasswordHasher->hashPassword(
-        //             $user,
-        //             $form->get('plainPassword')->getData()
-        //         )
-        //     );
-
-        //     $entityManager->persist($user);
-        //     $entityManager->flush();
-
-        //     // do anything else you need here, like send an email
-
-        //     return $this->redirectToRoute('_profiler_home');
-        // }
-
-        // return $this->render('registration/register.html.twig', [
-        //     'registrationForm' => $form,
-        // ]);
     }
 }
--- API/src/Controller/SecurityController.php
+++ API/src/Controller/SecurityController.php
@@ -5,22 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    #[Route(path: '/login', name: 'app_login', methods: ['POST'])]
+    public function login(Request $request): Response
     {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
 
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->json($request);
+        // // get the login error if there is one
+        // $error = $authenticationUtils->getLastAuthenticationError();
 
-        return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
+        // // last username entered by the user
+        // $lastUsername = $authenticationUtils->getLastUsername();
+
+        $user = $this->getUser();
+
+        return $this->json([
+            'user' => $user,
         ]);
     }
 
